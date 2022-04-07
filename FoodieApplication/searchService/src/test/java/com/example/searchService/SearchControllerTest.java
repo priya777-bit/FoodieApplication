83ee0e32ead53337c6561b40e8a9f002bb0d12b4
priyanshic83@gmail.com <priyanshic83@gmail.com>
2022-04-07 13:06:30 +0530

    commit 21

--- FoodieApplication/searchService/src/test/java/com/example/searchService/SearchControllerTest.java
+++ FoodieApplication/searchService/src/test/java/com/example/searchService/SearchControllerTest.java
@@ -1,23 +1,23 @@
-package com.example.searchService;
-
-import com.example.searchService.controller.SearchController;
-import com.example.searchService.service.SearchService;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
-
-@ExtendWith(MockitoExtension.class)
-public class SearchControllerTest {
-
-    @InjectMocks
-    private SearchController searchController;
-
-    @Mock
-    private SearchService searchService;
-
-    @Autowired
-    private MockMvc mockMvc;
-}
+//package com.example.searchService;
+//
+//import com.example.searchService.controller.SearchController;
+//import com.example.searchService.service.SearchService;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class SearchControllerTest {
+//
+//    @InjectMocks
+//    private SearchController searchController;
+//
+//    @Mock
+//    private SearchService searchService;
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//}
