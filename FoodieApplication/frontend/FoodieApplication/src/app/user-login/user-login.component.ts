670e88c3f59c3c01303c1f510afd9caae3012cbd
twinkledahale98@gmail.com <twinkledahale98@gmail.com>
2022-04-08 11:47:23 +0530

    commit 16

--- FoodieApplication/frontend/FoodieApplication/src/app/user-login/user-login.component.ts
+++ FoodieApplication/frontend/FoodieApplication/src/app/user-login/user-login.component.ts
@@ -52,7 +52,7 @@ export class UserLoginComponent {
         this.data=response;
         this.request.token=this.data.token;
         console.log(this.data.token);
-        this.authServe.loginUser(null);
+        this.authServe.loginUser(this.data.token);
         this.request.mailId=data.userMailId;
         const value=this.authServe.logIn(data.loginType); // null
         //this.request.mailId=this.fav.userMailId;
